89930321f78443961316d1b877cc9f3c0800a593
johnmoutafis <jg2moutafis@gmail.com>
2019-09-20 01:50:32 +0300

    Power Map: Clean up redundant npm modules.

--- webpack.base.config.js
+++ webpack.base.config.js
@@ -4,7 +4,6 @@ module.exports = {
   module: {
     rules: [
       {test: /\.(js|jsx)$/, exclude: /node_modules/, loaders: ['babel-loader']},
-      {test: /\.css$/, loaders: ['style-loader', 'css-loader']},
       {test: /\.(png|jpg)$/, loaders: ['url-loader']}
     ]
   },
